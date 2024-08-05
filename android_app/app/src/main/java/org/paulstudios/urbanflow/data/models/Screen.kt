e6e8975396c99e074c20d8e286d5d6f2f1d84080
HilFing <81067543+hilfing@users.noreply.github.com>
2024-08-05 14:53:33 +0530

    Delete android_app directory

--- android_app/app/src/main/java/org/paulstudios/urbanflow/data/models/Screen.kt
+++
@@ -1,9 +0,0 @@
-package org.paulstudios.urbanflow.data.models
-
-sealed class Screen(val route: String) {
-    object Login : Screen("login")
-    object Register : Screen("register")
-    object InfoScreen : Screen("info_screen")
-    object MapScreen : Screen("map_screen")
-    object RegisterDetails : Screen("register_details")
-}
